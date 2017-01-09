e2864b6c45b701f47597e62890aedcf73515c164
omarcoteixeira <omarcoteixeira@gmail.com>
2017-01-09 13:27:05 -0200

    Adding vscode configuration and updating materialui references.

--- api/definition.py
+++ api/definition.py
@@ -1,11 +1,10 @@
 """Main API definition."""
 import endpoints
+
 from settings import WEB_CLIENT_ID
-from settings import FIREBASE_GOOGLE_AUTH_CLIENT_ID
 
 ALLOWED_CLIENT_IDS = [endpoints.API_EXPLORER_CLIENT_ID,
-                      WEB_CLIENT_ID,
-                      FIREBASE_GOOGLE_AUTH_CLIENT_ID]
+                      WEB_CLIENT_ID]
 
 api_definition = endpoints.api(name='scaffold', version='v1',
                                description='Scaffold API',
